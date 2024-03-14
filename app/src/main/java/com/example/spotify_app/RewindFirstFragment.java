8bd9df3a7ca70a575c5876107cca84e069b3e326
jrh4567 <jacksonholbrook@hey.com>
2024-03-14 13:37:50 -0400

    Added fragment template for first rewind view

---
+++ app/src/main/java/com/example/spotify_app/RewindFirstFragment.java
@@ -0,0 +1,36 @@
+package com.example.spotify_app;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RewindFirstFragment extends Fragment {
+
+    private RewindFirstViewModel mViewModel;
+
+    public static RewindFirstFragment newInstance() {
+        return new RewindFirstFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rewind_first, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RewindFirstViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/spotify_app/RewindFirstViewModel.java
@@ -0,0 +1,7 @@
+package com.example.spotify_app;
+
+import androidx.lifecycle.ViewModel;
+
+public class RewindFirstViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
